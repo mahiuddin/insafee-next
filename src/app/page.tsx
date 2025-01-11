c7791769271a5571c499145a630ef1885b961894
mahi uddin <mahi.ewu@gmail.com>
2025-01-11 22:25:53 +0600

    page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div class="container mx-auto">
+    <div className="container mx-auto">
       <h1>Insafee.com</h1>
       <h2>Bangladeshi book shop.</h2>
     </div>
